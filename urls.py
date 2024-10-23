4b2292e21f7375f3259a4d541accd219d4076cda
Renato D. Santos Jr. <renatodsantosjr9@gmail.com>
2024-10-23 22:51:04 +0800

    "eto ba yung setup mo ngayoN?"

--- urls.py
+++ urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+# san to?
+# asan yung home mo? yung pinakita ko sayo
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
